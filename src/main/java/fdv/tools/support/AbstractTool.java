40c17bbcec89e7210c0e61f0abf89223f6d69aea
Dmitry <dmitriy3141592604@gmail.com>
2017-12-25 11:47:59 +0500

    AbstractTool задокументирован

--- src/main/java/fdv/tools/support/AbstractTool.java
+++ src/main/java/fdv/tools/support/AbstractTool.java
@@ -5,8 +5,12 @@ import java.util.function.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.github.typemarkup.Responsibility;
+
+@Responsibility("Формирует базу для создания инструмента")
 public abstract class AbstractTool<ContextType> implements Consumer<String[]> {
 
+	@Responsibility("Предоставляет доступ до системы логирования")
 	protected final Logger logger = LoggerFactory.getLogger(getClass());
 
 }
